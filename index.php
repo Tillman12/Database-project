6c5aa383706d70b3d318ab6c713689f352f8c820
Tory Steining <vjs5xp@virginia.edu>
2016-04-27 14:51:01 -0400

    added style to search page, updated home page

--- index.php
+++ index.php
@@ -9,9 +9,13 @@
 <body>
 <div class="form">
 <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+<br>
+<p><a href="search.html">Search the Database</a></p>
+<p><a href="download.html">Download a Table</a></p>
+<br>
 <p><a href="add_ad.php">Add an Athletic Director</a></p>
 <p><a href="edit_db.php">Update the Database</a></p>
-<p><a href="search.html">Search the Database</a></p>
+<br>
 <a href="logout.php">Logout</a>
 </div>
 </body>
